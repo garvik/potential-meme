3cdd3e681e142bfc7534612eb79ebc11addc2321
garvik <garvik@gmail.com>
2019-02-21 14:55:37 +0100

    react-helmet added

--- server/server/client/src/App.tsx
+++ server/server/client/src/App.tsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Helmet } from "react-helmet";
 import "./App.css";
 import logo from "./logo.svg";
 
@@ -6,6 +7,15 @@ class App extends Component {
   public render() {
     return (
       <div className="App">
+        <Helmet>
+          <meta charSet="utf-8" />
+          <meta
+            name="Description"
+            content="Funny shit I found on the Internet"
+          />
+          <title>Garvik</title>
+          <link rel="shortcut icon" href={require("./favicon.ico")} />
+        </Helmet>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
         </header>
